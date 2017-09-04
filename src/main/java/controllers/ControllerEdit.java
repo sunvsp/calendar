01ffb5876ab92aabf2605bd99d9297b7d1ede47f
sunvsp <vasupol.ch@ku.th>
2017-09-04 12:31:06 +0700

    Modify minute

--- src/main/java/controllers/ControllerEdit.java
+++ src/main/java/controllers/ControllerEdit.java
@@ -24,13 +24,11 @@ public class ControllerEdit extends Observable {
     private Appointment appointment;
 
     private ResourceAppointment resourceAppointment = ResourceAppointment.getInstance();
-    private ObservableList<String> itemP = FXCollections.observableArrayList("None","!","!!","!!!");
+    private ObservableList<String> itemP = FXCollections.observableArrayList("None","!!!","!","!");
     private ObservableList<String> itemH = FXCollections.observableArrayList("00","01","02","03","04",
             "05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23");
-    private ObservableList<String> itemM = FXCollections.observableArrayList("00","01","02","03","04",
-            "05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23",
-            "24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41",
-            "42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59");
+    private ObservableList<String> itemM = FXCollections.observableArrayList("00","05","10","15","20",
+            "25","30","35","40","45","50","55","50","55");
 
 
     @FXML private Button submit;
